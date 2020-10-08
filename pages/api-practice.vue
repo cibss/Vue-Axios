f623542faaf0404f57a18cefd98c92a6fc7391c5
Seiba Shonia <cibss13@gmail.com>
2020-10-08 19:28:13 +0700

    init

---
+++ pages/api-practice.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div>
+        <h1>API Practice</h1>
+        <p>{{ testimonial.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios }) {
+        const testimonial  = await $axios.get('http://localhost:3000/api/fe/web/testimonials')
+        return {
+            testimonial
+        }
+    }
+}
+
+
+
+
+
+</script>
+
+<style>
+    .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    }
+</style>
